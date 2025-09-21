cc47609da9883763b4cbf7ffd7c50c53844211ff
Poojitha Nagilla <103567616+Pooja290702@users.noreply.github.com>
2025-09-21 18:19:30 -0500

    intro to pandas

---
+++ importing_pandas.py
@@ -0,0 +1,6 @@
+import pandas as pd
+data={'names':['John', 'Jordan', 'Joseph', 'David'], 'marks':{[90,100,65,72]}
+d=pd.DataFrame(data, index=[1,2,3,4])
+print(d)
+
+# in the above code, the dictionary 'data' is converted to a table using pandas.
