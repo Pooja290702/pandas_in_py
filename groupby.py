c3096f532e2f1eeb84d3f1804348328436385b5f
Poojitha Nagilla <103567616+Pooja290702@users.noreply.github.com>
2025-10-02 12:07:15 -0500

    Create groupby.py

---
+++ groupby.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+df={'city':['a','b','c','d','e','f'],'product':['q','w','e','r','t','y'],'sales':[10,20,30,40,50,60]}
+a=pd.DataFrame(df)
+grouped=a.groupby('city')['sales'].sum()
+print(grouped)
